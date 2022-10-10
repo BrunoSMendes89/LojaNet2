2c95028382467cd064ac95644da3ca88687aeca4
bruno.mendes <bruno.mendes@NB_MKBB_74>
2022-10-10 19:57:45 -0300

    Adicionar arquivos de projeto.

---
+++ LojaNet.Models/IClienteDados.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LojaNet.Models
+{
+    public interface IClienteDados
+    {
+        void Incluir(Cliente cliente);
+        void Alterar(Cliente cliente);
+        void Excluir(string id);
+        List<Cliente> ObterTodos();
+        Cliente ObterPorId(string id);
+        Cliente ObterPorEmail(string email);
+
+
+    }
+}
